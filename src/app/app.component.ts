d8cca696d4cfa5b063e2726a3ff5201ca404c303
Madasamy Mariappan <mada2385@ksplad.com>
2018-08-29 20:05:50 +0530

    First message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'image-uploader';
+  images: any = [];
+  selectedFiles: any = [];
+  imagesMap = new Map<string, any>();
+  files: any = [];
+  counter = 0;
+  title = "image-uploader";
+
+  onChange(event: any, input: any) {
+    this.files = [].slice.call(event.target.files);
+
+    console.log(event);
+
+    if (this.files) {
+      this.files.map((f, index) => {
+        var reader = new FileReader();
+        this.selectedFiles[index] = f.name;
+        reader.onload = (event: ProgressEvent) => {
+          this.images[index] = (<FileReader>event.target).result;
+          this.imagesMap.set(f.name, (<FileReader>event.target).result);
+        };
+        reader.readAsDataURL(f);
+      });
+    }
+
+    input.value = this.selectedFiles.join(",");
+  }
+
+  changeOrder(event: any, input: any) {
+    this.selectedFiles = input.value.split(",");
+    // console.log("changed order" + this.selectedFiles.toString());
+  }
+  save(event: any, input: any) {
+    alert("images uploading order ---> " + this.selectedFiles.toString());
+    console.log("images" + this.selectedFiles.toString());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ImageUploadModule } from "angular2-image-upload";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ImageUploadModule.forRoot(),
+    ReactiveFormsModule ,
+    NgxGalleryModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
